8b4c33c225b2ea3d679ee3a8997c1a937d2111f1
rexkyng <timescam@duck.com>
2024-12-20 17:41:26 +0800

    add baseURL & switch default models to 4o-mini

--- server/config.ts
+++ server/config.ts
@@ -1,10 +1,11 @@
 import { Config } from "./types"
 
 export const config: Config = {
-  defaultRaycastModel: "openai-gpt-3.5-turbo",
+  defaultRaycastModel: "openai-gpt-4o-mini",
   apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.OPENAI_API_BASE_URL,
   temperature: 0.5,
   translate: {
-    model: "gpt-3.5-turbo"
+    model: "gpt-4o-mini"
   }
 }
